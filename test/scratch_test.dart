1e67073e2f04d9cdb69414ddc74dae83fcda6581
gabeduke <gabeduke@gmail.com>
2019-10-01 00:55:41 -0400

    fix test

--- test/scratch_test.dart
+++ test/scratch_test.dart
@@ -4,11 +4,12 @@ import 'package:http/testing.dart';
 import 'dart:convert';
 import 'package:level_ui/src/resources/api_provider.dart';
 
+const basePath = "localhost";
 
 void main(){
   test("Get_Level happy path", () async {
     //setup the test
-    final apiProvider = ApiProvider();
+    final apiProvider = initApi(basePath);
     apiProvider.client = MockClient((request) async {
       final mapJson = {'reading':4.23};
       return Response(json.encode(mapJson),200);
